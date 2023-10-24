d933584d4601859ea9d20936fdf3d942152ab77f
Luisito2312 <146202954+Luisito2312@users.noreply.github.com>
2023-10-24 16:28:47 +0200

    cambio

--- 02_Variables/Ejemplos/Ejemplo01.java
+++ 02_Variables/Ejemplos/Ejemplo01.java
@@ -13,5 +13,47 @@ public class Ejemplo01  {
     //TIpo short
     short miNumShort = 5000;
     System.out.println(miNumShort);
+
+    //Tipo long
+    long miNumLong = 20000L;
+    System.out.println(miNumLong);
+
+    //tipo float
+    float niNumFloat = 1.2f;
+    System.out.println(niNumFloat);
+
+    //tipo boolean
+    boolean esjavaDivertidio = true;
+    boolean esElProfeDivertido = false;
+    System.out.println("Es Java divertido? "  + esjavaDivertidio );
+    System.out.println("Es el profe divertido? " + esElProfeDivertido);
+
+
+    int numero = 5;
+
+    System.out.println("El valor de la variable es " + numero);
+
+    double x;
+    double y;
+
+    x = 7;
+    y = 25.75;
+
+    System.out.println("x vale: " + x);
+    System.out.println("y vale: " + y); 
+
+    int num1 = 9;
+    int num2 = 2;
+
+    //Utilizando el % para el módulo o resto de la división mientras se concatenan varias variables intercaladas
+    System.out.println("El resto de dividir " + num1 +" entre "+num2+" es: " + num1 % num2);
+
+    int a = 9;
+    int b = 2;
+
+    double division;
+
+    //Lo que hace el double, es castear la variable y lo que hace es básicamente cambiar de tipo de variable
+    System.out.println("el casting de la variable a es " + (double) a);
   }
 }
---
+++ 02_Variables/Ejercicios/Ejercicio1.java
@@ -0,0 +1,10 @@
+/*
+ Este código va de crear dos variables asignarles valores y hacer operaciones con ellas.
+ * 
+ * @author Luis Godino
+ */
+public class Ejercicio1 {
+  public static void main(String[] args) {
+    
+  }
+}
