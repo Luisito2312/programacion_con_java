a8e9df328564f162c97636881d60fb25bb3fda71
Luisito2312 <146202954+Luisito2312@users.noreply.github.com>
2023-11-10 19:05:17 +0100

    ejercicios 22 y 23

---
+++ 04_Sentencias_condicionales/Ejercicios/T04_Ejercicio22.java
@@ -0,0 +1,21 @@
+/*
+ Hacer un codigo que metiendo dia y hora y que salga por pantalla cuanto queda para el fin de semana
+ * 
+ * @author Luis Godino
+ */
+import java.util.Scanner;
+
+public class T04_Ejercicio22 {
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Introduce un día de la semana");
+    String dia = sc.nextLine();
+
+    System.out.println("Introduce una hora y ");
+    Double hora = sc.nextDouble();
+
+    
+  }
+}
---
+++ 04_Sentencias_condicionales/Ejercicios/T04_Ejercicio23.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class T04_Ejercicio23 {
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Introduzca el precio del artículo que va a comprar");
+    Double precio = sc.nextDouble();
+    
+    System.out.println("Introduce el tipo de IVA que quiere aplicar general, reducido, superreducido");
+    String tipoiva = sc.nextLine();
+    sc.nextLine();
+   
+    System.out.println("Tiene algún código promocional nopro, mitad, meno5, 5porc");
+    String tipodesc = sc.nextLine();
+
+    //Calculamos el IVA y el precio antes de usar coddigo promocional
+    int porcentajeiva = 0;
+    switch (tipoiva) {
+      case "general":
+        porcentajeiva = 21;
+        break;
+      
+      case "reducido":
+        porcentajeiva = 10;
+        break;
+       
+      case "superreducido":
+        porcentajeiva = 4;
+      default:
+        break;
+    }
+    
+    Double tipodescuento = 0.0;
+
+    switch (tipodesc) {
+      case "nopro":
+        tipodescuento = 0.0;
+        break;
+
+      case "mitad":
+        tipodescuento = precio / 2;
+        break;
+
+      case "meno5":
+        tipodescuento = 5.0;
+        break;
+
+      case "5porc":
+        tipodescuento = precio * 0.05;
+        break;  
+      default:
+        System.out.println("Has introducido un codigo promocional erróneo");
+        break;
+    }
+    System.out.println();
+  }
+}
