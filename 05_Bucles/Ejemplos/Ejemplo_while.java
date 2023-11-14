9ca8931ee3c34ace5dc09ec12de88e6cdd0a91c6
Luisito2312 <146202954+Luisito2312@users.noreply.github.com>
2023-11-14 16:23:15 +0100

    ejemplos while

---
+++ 05_Bucles/Ejemplos/Ejemplo_while.java
@@ -0,0 +1,16 @@
+/*
+ Sacar por pantalla del 1 al 10
+ * 
+ * @author Luis Godino
+ */
+public class Ejemplo_while {
+ public static void main(String[] args) {
+  
+  int i = 1;
+
+  while (i < 11) {
+    System.out.println(i);
+    i++;
+  }
+ } 
+}
---
+++ 05_Bucles/Ejemplos/Ejemplo_while2.java
@@ -0,0 +1,28 @@
+/*
+ Contador de números positivos introducidos por teclado
+ * 
+ * @author Luis Godino
+ */
+import java.util.Scanner;
+
+public class Ejemplo_while2 {
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+
+    int i = 0;
+    System.out.println("Introduce un número");
+      int num = sc.nextInt();
+
+    while (num > 0) {
+      System.out.println("Número introducido "+ num);
+      System.out.println("Introduzca otro número");
+      num = sc.nextInt();
+      i++;
+    }
+    System.out.println("Ha introducido un número real");
+    System.out.println("En total ha introducido "+i+ " números positivos");
+
+    sc.close();
+  }
+}
