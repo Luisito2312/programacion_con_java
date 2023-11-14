129dc6a8a42ac2091ac6c575279b32ab1f1f9d93
Luisito2312 <146202954+Luisito2312@users.noreply.github.com>
2023-11-14 15:37:31 +0100

    bucles

---
+++ 05_Bucles/Ejemplos/Ejemplo_for.java
@@ -0,0 +1,13 @@
+/*
+ Imprimir por pantalla del 1 al 10
+ * 
+ * @author Luis Godino
+ */
+public class Ejemplo_for {
+  public static void main(String[] args) {
+    
+    for (int i = 1; i < 11; i++) {
+      System.out.println(i);
+    }
+  }
+}
