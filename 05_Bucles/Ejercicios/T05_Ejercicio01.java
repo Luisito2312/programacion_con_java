b3dad19cfe8d1f48770e6446114397cff0a27820
Luisito2312 <146202954+Luisito2312@users.noreply.github.com>
2023-11-15 20:07:46 +0100

    sc

--- 05_Bucles/Ejercicios/T05_Ejercicio01.java
+++ 05_Bucles/Ejercicios/T05_Ejercicio01.java
@@ -1,9 +1,7 @@
 public class T05_Ejercicio01 {
   public static void main(String[] args) {
-
-    int num = 5;
     
-    for ( num = 5; num < 101; num +=5 ){
+    for ( int num = 0; num < 101; num +=5 ){
         System.out.println(num);
 
     }
