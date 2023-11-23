c54dea594f5b3388af932e376e7b6ccde4dc67cb
Luisito2312 <146202954+Luisito2312@users.noreply.github.com>
2023-11-23 19:58:20 +0100

    ejercicio333 tema 5

--- 05_Bucles/Ejercicios/T05_Ejercicio33.java
+++ 05_Bucles/Ejercicios/T05_Ejercicio33.java
@@ -1,6 +1,24 @@
+import java.util.Scanner;
+
 public class T05_Ejercicio33 {
   public static void main(String[] args) {
-    
-    Scanner
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Introduzca la altura de la U");
+    int altura = sc.nextInt();
+
+    for (int i = 0; i < altura -1; i++) {
+      System.out.print("*");
+      for (int j = 0; j < (altura -2); j++) {
+        System.out.print(" ");
+        
+      }
+      System.out.println("*");
+    }
+    System.out.print(" ");
+    for (int h = 0; h < altura - 2; h++) {
+      System.out.print("*");
+    }
+sc.close();
   }
 }
