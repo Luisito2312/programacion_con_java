66ec7f1b34126b95e503973372be38486b737cf6
Luisito2312 <146202954+Luisito2312@users.noreply.github.com>
2023-12-01 19:11:48 +0100

    historigrama

---
+++ 05_Bucles/Ejercicios/T05_Historigrama.java
@@ -0,0 +1,16 @@
+/*
+ Hacer un ejercicio que pida por teclado un carácter y, a continuación, una serie de números enteros (de 0 en adelante) hasta que se introduzca un número negativo. A continuación se mostrará una tabla donde en la primera columna aparecerán los números introducidos y en cada fila aparecerá repetido el carácter introducido tantas veces como indique la primera columna.
+ * 
+ * @author Luis Godino
+ */
+
+import java.util.Scanner;
+
+public class T05_Historigrama {
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+
+    
+  }
+}
