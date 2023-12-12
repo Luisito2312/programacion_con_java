930a8da9c79924f8589d8f0a0e74ed6361dc61f9
Luisito2312 <146202954+Luisito2312@users.noreply.github.com>
2023-12-12 10:52:30 +0100

    intento de 36

--- 05_Bucles/Ejercicios/T05_Ejercicio36.java
+++ 05_Bucles/Ejercicios/T05_Ejercicio36.java
@@ -2,9 +2,31 @@ import java.util.Scanner;
 
 public class T05_Ejercicio36 {
   public static void main(String[] args) {
-    
-    Scanner sc = new Scanner(System.in);
+        
+        Scanner scanner = new Scanner(System.in);
 
-    
-  }
+        System.out.print("Por favor, introduzca un número: ");
+        long numero = scanner.nextLong();
+
+        scanner.close();
+
+        // Verificar si el número es capicúo
+        if (esCapicua(numero)) {
+            System.out.println("El número " + numero + " es capicúo.");
+        } else {
+            System.out.println("El número " + numero + " no es capicúo.");
+        }
+    }
+
+    // Método para verificar si un número es capicúa
+    static boolean esCapicua(long numero) {
+        // Convertir el número a una cadena para facilitar la comparación
+        String numeroStr = Long.toString(numero);
+
+        // Comparar la cadena original con su reverso
+        String reverso = new StringBuilder(numeroStr).reverse().toString();
+
+        return numeroStr.equals(reverso);
+    }
 }
+
