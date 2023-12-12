f70c0206b562588f4127d42817cfa9dd074f2827
Luisito2312 <146202954+Luisito2312@users.noreply.github.com>
2023-12-12 11:05:19 +0100

    ejercicio 39

---
+++ 05_Bucles/Ejercicios/T05_Ejercicio39.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class T05_Ejercicio39 {
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+
+    int resltado = 1;
+    int salva = 0;
+
+    System.out.println("Introduzca un número entero");
+    int n = sc.nextInt();
+
+    for (int i = 1; i < n+1; i++) {
+      salva = i;
+      resltado *= salva;
+      System.out.println(i+"! = "+resltado);
+      
+    }
+  }
+}
