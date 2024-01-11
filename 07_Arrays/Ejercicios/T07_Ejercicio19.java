a1fd85e6d7a598832cbcfd571c5100da1f49098d
Luisito2312 <146202954+Luisito2312@users.noreply.github.com>
2024-01-11 21:19:21 +0100

    array

--- 07_Arrays/Ejercicios/T07_Ejercicio19.java
+++ 07_Arrays/Ejercicios/T07_Ejercicio19.java
@@ -16,13 +16,29 @@ public class T07_Ejercicio19 {
       System.out.printf("%5d", num[i]);
     }
 
-    System.out.print("Introduzca el númro que quiere insertar");
+    System.out.print("\nIntroduzca el número que quiere insertar ");
     int numeroIntroducido = Integer.parseInt(System.console().readLine());
 
-    System.out.print("Introduce la posición donde quiere insertar el número (0-11)");
+    System.out.print("Introduce la posición donde quiere insertar el número (0-11) ");
     int posicion = Integer.parseInt(System.console().readLine());
 
     //Desplazar los valores e insertar los números
-    
+    for (int i = num.length - 1; i < posicion; i--) {
+      num[i] = num[i - 1];
+
+    }
+    num[posicion] = numeroIntroducido;
+
+    //Mostramos por pantalla el resultado 
+    System.out.println("Array Modificado: ");
+    System.out.print("Índice ");
+    for (int i = 0; i < num.length; i++) {
+      System.out.printf("%5d", i);
+    }
+
+    System.out.print("\nValor  ");
+    for (int i = 0; i < num.length; i++) {
+      System.out.printf("%5d", num[i]);
+    }
   }
 }
