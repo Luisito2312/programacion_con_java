57c699690c9366af1c9a1c7ed3279e5463fe56b3
Luisito2312 <146202954+Luisito2312@users.noreply.github.com>
2024-01-16 16:35:19 +0100

    poo

---
+++ 09_POO/Ejemplos/Coche.java
@@ -0,0 +1,25 @@
+/*
+ Clase de ejemplo coche
+ * 
+ * @author Luis Godino
+ */
+public class Coche {
+
+  //Atributos
+  String matricula;
+  String marca;
+  String modelo;
+  int puertas;
+  String color;
+  int potencia;
+
+  public static void main(String[] args) {
+    //formula : Nombre_clase nombre_objeto = new Nombre_Clase() 
+    Coche miCoche = new Coche(); //Coche 1
+    Coche tuCoche = new Coche(); //Coche 2
+
+    System.out.println("Matricula del coche 1: "+ miCoche.matricula);
+    System.out.println("La potencia del coche 2 es: " + tuCoche.potencia);
+
+  }
+}
